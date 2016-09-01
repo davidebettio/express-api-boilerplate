8d5a40dfd7fb97aae840303ae72b93c88ffd5d03
Davide Bettio <davide.bettio@gmail.com>
2016-09-01 12:18:21 +0200

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import winston from 'winston';
+import morgan from 'morgan';
+import { hostname } from 'os';
+import compression from 'compression';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 'loopback');
+  app.use(morgan('common'));
+} else {
+  app.use(morgan('dev'));
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+
+app.get('/', (req, res) => {
+  res.send({ welcome_to: 'gtroutes-api' });
+});
+
+const server = app.listen(8001, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  winston.info(`HOSTNAME: ${hostname()}`);
+  winston.info(`NODE_ENV: ${app.get('env')}`);
+  winston.info(`app listening at http://${host}:${port}`);
+});
